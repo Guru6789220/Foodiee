a2b8ab9bdb30a8b6a7353c358532b71fa81beb99
Guru Prasad K N <guruprasadkn29206@gmail.com>
2024-09-03 23:11:54 +0530

    File Upload -- Add New category

---
+++ Foodiee.FrontEnd/Controllers/SellersController.cs
@@ -0,0 +1,34 @@
+﻿using Foodiee.FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foodiee.FrontEnd.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Category()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Category(CategoryDTO categoryDTO)
+        {
+            try
+            {
+                if(!ModelState.IsValid)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            catch(Exception ex)
+            {
+                TempData["Error"] = ex.Message.ToString();
+            }
+           return View(categoryDTO);
+        }
+    }
+}
---
+++ Foodiee.FrontEnd/Models/CategoryDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Foodiee.FrontEnd.Models
+{
+    public class CategoryDTO
+    {
+        [Required]
+        public CategoryTable CategoryDetails { get; set; }
+
+        [Required]
+        public string? CreatedBy { get; set; }
+    }
+
+    public class CategoryTable
+    {
+        [Required(ErrorMessage ="Enter Category Name"),MaxLength(100)]
+        [DisplayName("Category Name")]
+        public string? CategoryName { get; set; }
+
+        [Required(ErrorMessage ="Enter Category Descryption"), MaxLength(500)]
+        [DisplayName("Category Descryption")]
+        public string? CategoryDesc { get; set; }
+
+        [Required(ErrorMessage ="Upload Image -- .PNG , .JPG , .JPEG , .GIF  And Size Should Be Less Than 10MB")]
+        [DisplayName("Category Image ")]
+        public IFormFile? CategoryImageURL { get; set; }
+
+
+    }
+}
